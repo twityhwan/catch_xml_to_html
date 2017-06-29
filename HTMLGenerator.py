13fcbe127a3a773bdda531c25290dd7c39069216
HwanHee Yang <twityhwan@alticast.com>
2017-06-29 16:26:46 +0900

    Improved code readability and usage.

--- HTMLGenerator.py
+++ HTMLGenerator.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python2.7
 import xml.etree.ElementTree as ET
+import sys
+import os.path
 
-xmlFileName = 'out.xml'
+xmlFileName = ''
 outFileName = 'result.html'
+
+def isFileExist(path):
+    return os.path.isfile(path)
+
+def printUsage():
+    print 'Usage(1): ./HTMLGenerator.py [xml_file]'
+    print 'output file is result.html'
+    print 'Usage(2): ./HTMLGenerator.py [xml_file] -o [html_file]'
+    print '[Option]'
+    print '-o : set the output file name'
+    exit(1)
+
+if len(sys.argv) == 2:
+    if os.path.isfile(sys.argv[1]):
+        xmlFileName = sys.argv[1]
+    else:
+        printUsage()
+elif len(sys.argv) == 4:
+    xmlFileName = sys.argv[1]
+    outFileName = sys.argv[3]
+else:
+    printUsage()
+
 xmlText = ET.parse(xmlFileName);
-htmlDocText = '''<!DOCTYPE html>
+htmlDocText = """<!DOCTYPE html>
 <html>
 <head>
 {style}
@@ -13,7 +38,7 @@ htmlDocText = '''<!DOCTYPE html>
 <body>
 {contents}
 </body>
-</html>'''
+</html>"""
 
 def getThStyle(tag):
     style = ''
@@ -32,27 +57,29 @@ def getTdHTML(key, val):
     style_=''
     if key == "success":
         if val == "true":
-            style_='style="color: blue;"'
+            style_ = 'style="color: blue;"'
         else:
-            style_='style="color: red; font-weight:bold;"'
+            style_ = 'style="color: red; font-weight:bold;"'
     elif key == "failures" and val > 0:
-        style_='style="color: red; font-weight:bold;"'
-    html = html.format(style=style_)
+        style_ = 'style="color: red; font-weight:bold;"'
+    html = html.format(style = style_)
     return html
 
 def convertToHTML(node):
-    html='<table style="width:100%"><th '+getThStyle(node.tag)+' colspan="'+\
-    str(len(node.keys())*2)+'">'+node.tag+'</th>{rows}</table>'
+    html = ('<table style="width:100%"><th ' + getThStyle(node.tag)
+            + ' colspan="' + str(len(node.keys())*2) + '">'
+            + node.tag
+            + '</th>{rows}</table>')
     tds = ''
     for key in node.keys():
-        tds+=getTdHTML(key, node.attrib[key])
-    html = html.format(rows = '<tr>'+tds+'</tr>{rows}')
+        tds += getTdHTML(key, node.attrib[key])
+    html = html.format(rows = ('<tr>' + tds + '</tr>{rows}'))
     tds = ''
     if type(node.text) is str:
         text = node.text.strip()
         if text != "":
-            tds += '<td colspan="'+str(len(node.keys())*2)+'">'+\
-            node.text.strip()+'</td>'
+            tds += ('<td colspan="' + str(len(node.keys())*2) + '">'
+                + node.text.strip() + '</td>')
     html = html.format(rows = tds)
     return html
 
@@ -64,7 +91,7 @@ def xmlIter(root):
     return html
 
 # Set style
-cssStyle = '''<style>
+cssStyle = """<style>
 table, th, td {border: 1px solid black;border-collapse: collapse;}
 th, td {padding: 5px;text-align: left;}
 th.group {background-color: #00131a; color: white;}
@@ -72,7 +99,7 @@ th.testcase {background-color: yellow;}
 th.overallresult {background-color: #80ff80;}
 th.overallresults {background-color: #8080ff;}
 th {background-color: #ffffcc;}
-</style>'''
+</style>"""
 
 # Convert xml to html
 root = xmlText.getroot()
